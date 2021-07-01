53b145b068b29d68cfeca1b502a9693ced68ae10
Mangalsing1995 <86702539+Mangalsing1995@users.noreply.github.com>
2021-07-01 08:11:56 +0530

    Update BindasTest.java
    Commit Form Remote Repository

--- WelcomeTest/src/com/welcome/BindasTest.java
+++ WelcomeTest/src/com/welcome/BindasTest.java
@@ -3,5 +3,8 @@ package com.welcome;
 public class BindasTest {
   public static void main(String arge){
     System.out.println("Welcome to indasTest");
+    System.out.println("My Name is Mangsling");   
+    System.out.println("My Name is Ashok Pandit");
+    System.out.println("My Name is Vikas Bingare");
   }
-}
+}   
